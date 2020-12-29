5f4fa587c25d2f21b1303c03ee01bcc950ee4893
Kevin Clement <kevin.clement@gmail.com>
2020-12-29 00:08:51 -0800

    write and test light sensor

--- light-sensor/src/main.cpp
+++ light-sensor/src/main.cpp
@@ -1,29 +1,45 @@
 #include <Arduino.h>
 
+#define LS_PIN A11
+#define LS_THRESHOLD 750
+
+bool sawLight = false;
+bool showLightValues = false;  // switch to turn on dump of light values
+
 void setup() {
 
   Serial.begin(9600);
+  Serial.setTimeout(10);
   Serial.println("Light Sensor...");
 
-  pinMode(A0, INPUT_PULLUP);
-  pinMode(A1, INPUT_PULLUP);
-  pinMode(A2, INPUT_PULLUP);
-  pinMode(A3, INPUT_PULLUP);
+  pinMode(LS_PIN, INPUT_PULLUP);
+}
+
+void loop() { 
+
+  bool light = analogRead(LS_PIN) < LS_THRESHOLD;
+  if (light && !sawLight) {
+    sawLight = true;
+    Serial.println("I SAW LIGHT!!!!");
   }
 
-void loop() {
-  // ## LIGHT SENSOR ##########
-  
-  // int average = 0;
-  // for (int i=0; i < 10; i++) {
-  //   average = average + analogRead(A1);
-  // }
-  // average = average/10;
+  if (Serial.available()) {
+    String str = Serial.readString();
+    showLightValues = !showLightValues;
+    
+    Serial.print("Show Light: ");
+    if (showLightValues) {
+      Serial.print("ON");
+    } else {
+      Serial.print("OFF");
+    }
+    Serial.println();
+  }
 
-  // Serial.print(" A1: ");
-  // Serial.print(average);
-
-  Serial.println("");
+  if (showLightValues) {
+    Serial.print("light: ");
+    Serial.println(analogRead(LS_PIN));
+  }
   
   delay(100);
 }
