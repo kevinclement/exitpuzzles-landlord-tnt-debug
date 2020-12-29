509e68fd170dc6119a4da18e2f80990b8b7dd694
Kevin Clement <kevin.clement@gmail.com>
2020-12-28 23:59:58 -0800

    write and test door ajar button

--- door-ajar/src/main.cpp
+++ door-ajar/src/main.cpp
@@ -1,12 +1,31 @@
 #include <Arduino.h>
 
+#define DOORBTN 8
+#define DOOR_OPEN LOW
+#define DOOR_CLOSED HIGH
+
+bool doorOpen = false;
 void setup() {
 
   Serial.begin(9600);
+  Serial.setTimeout(10);
   Serial.println("Door Ajar Button...");
+
+  pinMode(DOORBTN, INPUT_PULLUP);
 }
 
 void loop() {
-  // Serial.println("");  
+  int doorBtnReading = digitalRead(DOORBTN);
+  
+  if (doorBtnReading == DOOR_OPEN && !doorOpen) {
+    Serial.println("Door OPENED!");
+    doorOpen = true;
+  } 
+
+  if (doorBtnReading == DOOR_CLOSED && doorOpen) {
+    Serial.println("Resetting door to closed state.");
+    doorOpen = false;
+  }
+
   delay(100);
 }
