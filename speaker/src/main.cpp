9e4c299e8633be3200966c3a120ad568eac6b0b4
Kevin Clement <kevin.clement@gmail.com>
2020-12-28 22:22:28 -0800

    add test for speaker pizo

--- speaker/src/main.cpp
+++ speaker/src/main.cpp
@@ -1,12 +1,53 @@
 #include <Arduino.h>
 
+#define BUZZ A10
+
+void beep(unsigned char speakerPin, int frequencyInHertz, long timeInMilliseconds)
+{
+  int x;   
+  long delayAmount = (long)(1000000/frequencyInHertz); 
+  long loopTime = (long)((timeInMilliseconds*1000)/(delayAmount*2)); 
+  for (x=0;x<loopTime;x++)   
+  {   
+      digitalWrite(BUZZ,HIGH); 
+      delayMicroseconds(delayAmount); 
+      digitalWrite(BUZZ,LOW); 
+      delayMicroseconds(delayAmount); 
+  }
+}
+
+void uhoh() {
+  beep(BUZZ,415,100);
+  delay(80);
+  beep(BUZZ,279,100);
+  delay(80);
+}
+
+bool isSerial() {
+  bool available = false;
+
+  if (Serial.available()) {
+    String str = Serial.readString();
+    available = true;
+  }
+
+  return available;
+}
+
 void setup() {
 
   Serial.begin(9600);
+  Serial.setTimeout(10);
   Serial.println("Speaker PIZO...");
+
+  pinMode(BUZZ, OUTPUT);
+  digitalWrite(BUZZ, LOW); //Buzzer Off 
 }
 
 void loop() {
   // Serial.println("");  
-  delay(100);
+  if (isSerial()) {
+    Serial.println("Playing uhoh...");
+    uhoh();
+  }
 }
