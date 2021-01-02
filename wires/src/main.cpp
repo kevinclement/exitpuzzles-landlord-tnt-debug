82b4cd860fead970913be4f22280be20a0aca2fb
Kevin Clement <kevin.clement@gmail.com>
2021-01-01 20:15:31 -0800

    map source and destination of wires

--- wires/src/main.cpp
+++ wires/src/main.cpp
@@ -11,6 +11,26 @@ void setup() {
   pinMode(A3, INPUT_PULLUP);
 }
 
+const char * determineSource(int sig) {
+  const char * src;
+
+  if (sig > 0 && sig < 75) {
+    src = "C";
+  } else if (sig > 100 && sig < 175) {
+    src = "B";
+  } else if (sig > 200 && sig < 275) {
+    src = "A";
+  } else if (sig > 700 && sig < 775) {
+    src = "1";
+  } else if (sig > 800) {
+    src = "UPLG";
+  } else {
+    src = "UKWN";
+  }
+
+  return src;
+}
+
 void loop() {
   // ## WIRES ##########
   // left side (start) of wire always be the one that is ground
@@ -21,14 +41,39 @@ void loop() {
   // B->D : A1 (4.7k resistor - 125 avg reading)
   // C->2 : A3 (no resistor - 15 avg reading)
 
-  Serial.print("A0: ");
-  Serial.print(analogRead(A0));
-  Serial.print("   A1: ");
-  Serial.print(analogRead(A1));
-  Serial.print("   A2: ");
-  Serial.print(analogRead(A2));
-  Serial.print("   A3: ");
-  Serial.print(analogRead(A3));
+  // Source controls the signal strength
+  //  1: 700-775
+  //  A: 200-275
+  //  B: 100-175
+  //  C: 0-75
+
+  // ## Example 2 Destination
+  // #######################
+  // 1->2: 745
+  // A->2: 235
+  // B->2: 135
+  // C->2: 20 * PASSING
+
+  bool printRaw = false;
+  if (printRaw) {
+    Serial.print("   2: ");
+    Serial.print(analogRead(A3));
+    Serial.print("   3: ");
+    Serial.print(analogRead(A2));
+    Serial.print("   4: ");
+    Serial.print(analogRead(A0));
+    Serial.print("   D: ");
+    Serial.print(analogRead(A1));
+  } else {
+    Serial.print("   2: ");
+    Serial.print(determineSource(analogRead(A3)));
+    Serial.print("   3: ");
+    Serial.print(determineSource(analogRead(A2)));
+    Serial.print("   4: ");
+    Serial.print(determineSource(analogRead(A0)));
+    Serial.print("   D: ");
+    Serial.print(determineSource(analogRead(A1)));
+  }
   
   Serial.println();
   
